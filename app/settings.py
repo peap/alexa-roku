c877e63255561f0bf43299eb0c11821620ab6fcb
Eric Petersen <eric@huskers.unl.edu>
2016-11-03 23:11:56 -0700

    Get AMAZON_APPLICATION_ID and ROKU_ADDRESS from environment.
    Add some utterances.
    
    Bump version to 0.2.0.

--- app/settings.py
+++ app/settings.py
@@ -1,11 +1,11 @@
 import os
 
-AMAZON_APPLICATION_ID = ''
+AMAZON_APPLICATION_ID = os.environ.get('AMAZON_APPLICATION_ID', '')
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SKILL_INVOCATION_NAME = 'the Roku'
 SKILL_NAME = 'My Roku'
-SKILL_VERSION = '0.1.0'
+SKILL_VERSION = '0.2.0'
 
-ROKU_ADDRESS = ''
+ROKU_ADDRESS = os.environ.get('ROKU_ADDRESS', '')
